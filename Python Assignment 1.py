4608a728c685d8c91062dbe90183921baaa6066e
Lucas Leung <66652566+mrworldwide1@users.noreply.github.com>
2023-09-14 09:52:13 -0400

    Update Python Assignment 1.py

--- Python Assignment 1.py
+++ Python Assignment 1.py
@@ -3,8 +3,7 @@
 
 # Exercise 3.1
 # The cover price of a book is $24.95, but bookstores get a 40 percent discount.
-Shipping costs $3 for the first copy and 75 cents for each additional copy. Calculate the total
-wholesale costs for 60 copies.
+# Shipping costs $3 for the first copy and 75 cents for each additional copy. Calculate the total wholesale costs for 60 copies.
 
 # Book cover price
 coverPrice = 24.95
