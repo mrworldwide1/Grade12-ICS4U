74b0124ffbe667df7563d175eeb740f14c8481b3
Lucas Leung <66652566+mrworldwide1@users.noreply.github.com>
2023-10-07 18:55:21 -0400

    Update Python - Access List Item - W3Schools.py

--- Python - Access List Item - W3Schools.py
+++ Python - Access List Item - W3Schools.py
@@ -1,3 +1,11 @@
+# Explain in your own words how a programmer can access a range of index in a list.
+# Create some examples after seeing the 3 examples given in the link  
+
+# A programmer can access a range of index in a list by first typing the name of the list, followed by the range of index in square brackets. Don't forget to put everything in round brackets.
+# if you wish to access a single index in a list, you just need the single index number.
+# Example: myList[1] specifies second thing in the list
+# myList[0:2] specifies first to third item in the list
+
 def listRange(list):
   return(list[1:3])
 
